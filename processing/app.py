7d2cb9e41a5e18e3e06416dcac5f52a8167d782c
Goutam Thukral <goutamthukral@gmail.com>
2023-11-16 10:04:00 -0800

    done

--- processing/app.py
+++ processing/app.py
@@ -62,6 +62,8 @@ def populate_stats():
             "max_humidity_reading": 0,
             "last_updated": 0
         }
+        with open(filename, "w") as json_file:
+            json.dump(data, json_file)
 
     num_temperature_readings = data['num_temperature_readings']
     avg_max_temperature_reading = data['avg_max_temperature_reading']
